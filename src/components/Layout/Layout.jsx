6bc8ab713e5a1f882e3a385acd7bb425a7e5fba1
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-02-11 13:41:20 +0100

    Create error component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,11 +3,12 @@ import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Loader from '../Loader'
 
 const Layout = () => {
     return (
         <div>
-            <Suspense fallback={null}>
+            <Suspense fallback={<Loader />}>
                 <Outlet />
             </Suspense>
             <ToastContainer />

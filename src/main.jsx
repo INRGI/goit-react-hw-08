a6a9d221ad268c9a2542f30395d40cc0aab1a0e0
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-02-12 14:38:08 +0100

    Add Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import './index.css'
 import App from './components/App'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import Loader from './components/Loader'
+import { BrowserRouter } from 'react-router-dom'
+import { store, persistor } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={<Loader />} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
